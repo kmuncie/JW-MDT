98a54fa76c734d296b6e861c701eb57412e05151
Craig Weber <crgwbr@gmail.com>
2014-01-12 18:22:07 -0500

    Added Gruntfile for jshinting

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        jshint: {
+            all: ['*.js']
+        }
+    });
+
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    // Default task(s).
+    grunt.registerTask('default', ['jshint']);
+
+};
